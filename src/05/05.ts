fc1aa301fbf5d92376f76377b0460d6eb67c5092
Rankarusu <darkigfx@gmail.com>
2022-12-05 18:19:16 +0100

    solve part 2

--- src/05/05.ts
+++ src/05/05.ts
@@ -11,38 +11,64 @@ const instructionLines = instructions.split('\n');
 
 const crateTowerAmount = Array.from(crateLines[0].matchAll(crateRegex)).length;
 
-//initialize empty array
-const towers: string[][] = [];
-for (let i = 0; i < crateTowerAmount; i++) {
-  towers.push([]);
+function setupTowers() {
+  const crateTowers: string[][] = [];
+  for (let i = 0; i < crateTowerAmount; i++) {
+    crateTowers.push([]);
+  }
+  crateLines.forEach((line) => {
+    const crates = Array.from(line.matchAll(crateRegex));
+    for (let i = 0; i < crates.length; i++) {
+      const element = crates[i][0].trim();
+      if (element !== '') {
+        crateTowers[i].push(element);
+      }
+    }
+  });
+  return crateTowers;
+}
+function displayResult(towers: string[][]) {
+  const message = towers
+    .map((tower) => tower[0])
+    .join()
+    .replaceAll(/\[|\]|,/g, '');
+
+  console.log(message);
 }
 
-crateLines.forEach((line) => {
-  const crates = Array.from(line.matchAll(crateRegex));
-  for (let i = 0; i < crates.length; i++) {
-    const element = crates[i][0].trim();
-    if (element !== '') {
-      towers[i].push(element);
-    }
-  }
-});
+let towers: string[][] = setupTowers();
 
-instructionLines.forEach((line) => {
-  const match = line.match(instructionRegex);
-
-  const amount = parseInt(match![1]);
-  const from = parseInt(match![2]);
-  const to = parseInt(match![3]);
-
+function crateMove9k(amount: number, from: number, to: number) {
   for (let i = 0; i < amount; i++) {
-    const element = towers[from - 1].shift();
-    towers[to - 1].unshift(element!);
+    const element = towers[from].shift();
+    towers[to].unshift(element!);
   }
-});
+}
 
-const message = towers
-  .map((tower) => tower[0])
-  .join()
-  .replaceAll(/\[|\]|,/g, '');
+function carryOutInstructions(
+  instructions: string[],
+  crane: (amount: number, from: number, to: number) => void
+) {
+  instructions.forEach((line) => {
+    const match = line.match(instructionRegex);
 
-console.log(message);
+    const amount = parseInt(match![1]);
+    const from = parseInt(match![2]) - 1;
+    const to = parseInt(match![3]) - 1;
+
+    crane(amount, from, to);
+  });
+}
+
+carryOutInstructions(instructionLines, crateMove9k);
+
+displayResult(towers);
+
+//part 2
+towers = setupTowers(); //reset
+function crateMove9k1(amount: number, from: number, to: number) {
+  const elements = towers[from].splice(0, amount);
+  towers[to].unshift(...elements);
+}
+carryOutInstructions(instructionLines, crateMove9k1);
+displayResult(towers);

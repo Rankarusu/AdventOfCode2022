487136d6a2111e3a31ce20fcfe85ed102f9195bf
Rankarusu <darkigfx@gmail.com>
2022-12-10 13:25:02 +0100

    solve part 2

--- src/09/09.ts
+++ src/09/09.ts
@@ -5,7 +5,10 @@ const content = readFile(9);
 const instructions = content.split('\n');
 
 class Point {
+  public tail?: Point;
+
   constructor(public x: number = 0, public y: number = 0) {}
+
   isEqual(point: Point) {
     if (this.x === point.x && this.y === point.y) {
       return true;
@@ -15,63 +18,84 @@ class Point {
   move(direction: string) {
     switch (direction) {
       case 'U':
-        this.x++;
-        break;
-      case 'D':
-        this.x--;
-        break;
-      case 'R':
         this.y++;
         break;
-      case 'L':
+      case 'D':
         this.y--;
+        break;
+      case 'R':
+        this.x++;
+        break;
+      case 'L':
+        this.x--;
         break;
     }
   }
-  setPosition(x: number, y: number) {
-    this.x = x;
-    this.y = y;
+
+  follow(x: number, y: number) {
+    const xDistance = Math.abs(this.x - x);
+    const yDistance = Math.abs(this.y - y);
+
+    if (xDistance === 2) {
+      if (yDistance >= 1) {
+        if (this.y > y) {
+          this.move('D');
+        } else {
+          this.move('U');
+        }
+      }
+
+      if (this.x > x) {
+        this.move('L');
+      } else {
+        this.move('R');
+      }
+    } else if (yDistance === 2) {
+      if (xDistance >= 1) {
+        if (this.x > x) {
+          this.move('L');
+        } else {
+          this.move('R');
+        }
+      }
+
+      if (this.y > y) {
+        this.move('D');
+      } else {
+        this.move('U');
+      }
+    }
+    this.tail?.follow(this.x, this.y);
   }
 }
 
 const visitedPositions: Point[] = [];
 
-const head = new Point();
-const tail = new Point();
+const rope: Point[] = [];
 
-instructions.forEach((instruction, index) => {
+for (let i = 0; i < 10; i++) {
+  const point = new Point();
+  if (i > 0) {
+    rope[i - 1].tail = point;
+  }
+  rope.push(point);
+}
+
+instructions.forEach((instruction) => {
   const [direction, _amount] = instruction.split(' ');
   const amount = parseInt(_amount);
   for (let i = 0; i < amount; i++) {
-    const prevPos = new Point(head.x, head.y);
-    head.move(direction);
-    const xDistance = Math.abs(tail.x - head.x);
-    const yDistance = Math.abs(tail.y - head.y);
-    // console.log(xDistance, yDistance);
-    if (
-      (xDistance === 2 && yDistance === 0) ||
-      (xDistance === 0 && yDistance === 2)
-    ) {
-      tail.move(direction);
-      // console.log('move');
-    } else if (
-      (xDistance === 2 && yDistance === 1) ||
-      (xDistance === 1 && yDistance === 2)
-    ) {
-      tail.setPosition(prevPos.x, prevPos.y);
-      // console.log('jump');
-    }
-    console.log(index);
+    rope[0].move(direction);
+    rope[0].tail?.follow(rope[0].x, rope[0].y);
 
-    console.log(`head pos: ${head.x} ${head.y}`);
-    console.log(`tail pos: ${tail.x} ${tail.y}`);
     if (
       !visitedPositions.find(
-        (point) => point.x === tail.x && point.y === tail.y
+        (point) => point.x === rope[9].x && point.y === rope[9].y
       )
     ) {
-      visitedPositions.push(new Point(tail.x, tail.y));
+      visitedPositions.push(new Point(rope[9].x, rope[9].y));
     }
   }
 });
+
 console.log(visitedPositions.length);
